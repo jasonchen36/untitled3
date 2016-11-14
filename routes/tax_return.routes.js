8b592e0509aefedea3b0b0485d6e7618267e2c07
Jason <jason.chen@ellefsontech.com>
2016-11-14 16:34:06 -0500

    added more routes

--- routes/tax_return.routes.js
+++ routes/tax_return.routes.js
@@ -20,4 +20,20 @@ module.exports = function (router) {
         .get(tax_return.findById);
     router.route('/tax_return')
         .post(tax_return.create);
+    router.route('/tax_return/:id/answer')
+        .post(answers.create);
+    router.route('/tax_return/:id/answer/:id')
+        .get(answers.findById);
+    router.route('/tax_return/:id/answers')
+        .get(answers.find);
+    router.route('/tax_return/:id/address')
+        .post(address.create);
+    router.route('/tax_return/:id/address/:id')
+        .get(address.findById);
+    router.route('/tax_return/:id/checklist')
+        .get(checklist.find);
+    router.route('/tax_return/:id/document')
+        .post(document.create);
+    router.route('/tax_return/:id/document/:id')
+        .get(document.findById);
 };
