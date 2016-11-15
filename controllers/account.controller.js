a3f0c04367584ae0a14adaa67bd53e4223e27ee8
odonoghue <odonoghue.ml@gmail.com>
2016-11-15 14:42:29 -0500

    Documenting account endpoints

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -15,15 +15,41 @@ var logger = require('../services/logger.service');
 var _ = require('underscore');
 var config = require('../config/config');
 
+/*******************************************************************************
+ENDPOINT
+POST /account
 
+INPUT BODY:
+{
+  firstname:  "Michael",
+  product:    "2016"
+}
+
+RESPONSE:
+200 OK
+ ******************************************************************************/
 exports.create = function (req, res) {
     res.status(200).send('OK');
 };
 
+/*******************************************************************************
+ENDPOINT
+GET /account/:id
 
+INPUT BODY:
+None. req.params.id is the only input (no body)
+
+RESPONSE:
+{
+  userId: 913
+  name: "Frank"
+  email: "test@email.com"
+}
+*******************************************************************************/
 exports.findById = function (req, res) {
     var id = req.params.id;
     var jsonData = {
+      userId: 913,
       name: 'test',
       email: 'test@email.com'
     };
