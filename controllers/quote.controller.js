f70ec81873016e52e5c7a6356f494daf7f750c29
odonoghue <odonoghue.ml@gmail.com>
2016-11-16 15:27:39 -0500

    Adding sample output for GET /quote

--- controllers/quote.controller.js
+++ controllers/quote.controller.js
@@ -41,9 +41,63 @@ None. req.params.id is the only input (no body)
 
 RESPONSE:
 {
-  account_id: "33",
-  product_id: "44",
-  price: "200"
+   "id":1,
+   "accountId":2,
+   "productId":3,
+   "taxReturnsQuoted":[
+      {
+         "taxReturnId":1,
+         "name":"Carmela Doctor",
+         "itemCount":3,
+         "items":[
+            {
+               "text":"Tax Prep. Basic",
+               "value":80
+            },
+            {
+               "text":"Tax Prep. Investment Income",
+               "value":10
+            },
+            {
+               "text":"Tax Prep. Rental Income",
+               "value":10
+            }
+         ]
+      },
+      {
+         "taxReturnId":2,
+         "name":"Doug Doctor",
+         "itemCount":3,
+         "items":[
+            {
+               "text":"Tax Prep. Basic",
+               "value":80
+            },
+            {
+               "text":"Tax Prep. Investment Income",
+               "value":10
+            },
+            {
+               "text":"Tax Prep. Rental Income",
+               "value":10
+            }
+         ]
+      }
+   ],
+   "otherLineItems":  // quoteLineItems table (taxReturnId needs to be null - remove constraint)
+   [
+      {
+         "id":33,
+         "name":"Direct Deposit",
+         "itemCount":1,
+         "items":[
+            {
+               "text":"Direct Deposit",
+               "value":5
+            }
+         ]
+      }
+   ]
 }
 *******************************************************************************/
 exports.findById = function (req, res) {
