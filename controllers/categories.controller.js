233446c7fee82c778ec487a394a9ab9e43e2c546
Jason <jason.chen@ellefsontech.com>
2016-12-02 12:51:32 -0500

    changed the comments for the categories controller

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -20,9 +20,26 @@ ENDPOINT
 GET /categories
 
 RESPONSE:
-{
 
-}
+[
+  {
+    "id": 1,
+    "name": "Income",
+    "displaytext": "Please add the following income information?",
+    "secondarytext": null,
+    "created_at": "2016-11-16T23:56:04.000Z",
+    "updated_at": "2016-11-17T22:15:44.000Z"
+  },
+  {
+    "id": 2,
+    "name": "Credits",
+    "displaytext": "Hi % do you have these credits:",
+    "secondarytext": null,
+    "created_at": "2016-11-16T23:56:11.000Z",
+    "updated_at": "2016-11-17T22:15:45.000Z"
+  }
+]
+
 *******************************************************************************/
 exports.list = function (req, res) {
 
