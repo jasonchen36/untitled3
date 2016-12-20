9f7c19b58f62e738ad6e781cb07dec3a126e8ea5
odonoghue <odonoghue.ml@gmail.com>
2016-12-20 13:07:45 -0500

    Adding Filers to GET /Checklist

--- models/checklist.model.js
+++ models/checklist.model.js
@@ -23,7 +23,7 @@ var Checklist = {
             return Promise.reject('No quoteId specified!');
         }
 
-        var checklistSQL = 'SELECT cr.checklist_item_id, ci.name FROM answers AS a \
+        var checklistSQL = 'SELECT DISTINCT cr.checklist_item_id, ci.name FROM answers AS a \
                             JOIN checklist_rules AS cr \
                                  ON a.question_id = cr.question_id \
                                  AND a.text = cr.value \
@@ -43,10 +43,11 @@ var Checklist = {
                 resultObj.additionalDocuments = [];
             }
 
-            var documentsSQL = 'SELECT * FROM documents AS d \
+            var documentsSql = 'SELECT * FROM documents AS d \
+                                JOIN tax_returns AS tr ON tr.id = d.tax_return_id \
                                 WHERE d.quote_id = ?;';
-            return db.knex.raw(documentsSQL, [quoteId]).then(function(documentsSQLResults) {
-                var dbDocs = documentsSQLResults[0];
+            return db.knex.raw(documentsSql, [quoteId]).then(function(documentsSqlResults) {
+                var dbDocs = documentsSqlResults[0];
                 var documents = [];
                 var docObj = {};
                 _.forEach(dbDocs, function(dbDoc) {
@@ -54,25 +55,56 @@ var Checklist = {
                     docObj.documentId = dbDoc.id;
                     docObj.quoteId = dbDoc.quote_id;
                     docObj.taxReturnId = dbDoc.tax_return_id;
+                    docObj.firstName = dbDoc.first_name;
+                    docObj.lastName = dbDoc.last_name;
                     docObj.checkListItemId = dbDoc.checklist_item_id;
                     docObj.name = dbDoc.name;
                     docObj.url = config.thumbnail.baseUploadUrl + dbDoc.url;
                     docObj.thumbnailUrl = config.thumbnail.baseThumbnailUrl + dbDoc.thumbnail_url;
                     documents.push(docObj);
                 });
-                _.forEach(checklistArr, function(checklistItem) {
-                    thisCheckListItemId = checklistItem.checklist_item_id;
-                    checklistItem.documents = _.filter(documents, function(o) {
-                        return o.checkListItemId === thisCheckListItemId;
-                    });
-                });
+                var filerSql = 'SELECT \
+                                  cr.checklist_item_id, \
+                                  ci.name, tr.id, \
+                                  tr.first_name, \
+                                  tr.last_name \
+                                FROM answers AS a \
+                                JOIN checklist_rules AS cr \
+                                  ON a.question_id = cr.question_id \
+                                  AND a.text = cr.value \
+                                JOIN checklist_items AS ci \
+                                  ON ci.id = cr.checklist_item_id \
+                                JOIN tax_returns AS tr \
+                                  ON tr.id = a.tax_return_id \
+                                WHERE a.tax_return_id IN( \
+                                   SELECT tax_return_id FROM quotes_tax_returns \
+                                   WHERE quote_id = ?)';
+                return db.knex.raw(filerSql, [quoteId]).then(function(filerSqlResults) {
+                    var dbFilers = filerSqlResults[0];
 
-                resultObj = {};
-                resultObj.checklistitems = checklistArr;
-                resultObj.additionalDocuments = _.filter(documents, function(o) {
-                    return o.checkListItemId === null; // no checklistItemId
-                });;
-                return(resultObj);
+                    _.forEach(checklistArr, function(checklistItem) {
+                        thisCheckListItemId = checklistItem.checklist_item_id;
+                        checklistItem.documents = _.filter(documents, function(o) {
+                            return o.checkListItemId === thisCheckListItemId;
+                        });
+                        checklistItem.filers = _.filter(dbFilers, function(o) {
+                            return o.checklist_item_id === thisCheckListItemId;
+                        });
+                        _.forEach(checklistItem.filers, function(filerObj) {
+console.log('filerObj = ' + JSON.stringify(filerObj, null, 2));
+                            delete filerObj.checklist_item_id;
+                            delete filerObj.name;
+                            delete filerObj.id;
+                        });
+                    });
+
+                    resultObj = {};
+                    resultObj.checklistitems = checklistArr;
+                    resultObj.additionalDocuments = _.filter(documents, function(o) {
+                        return o.checkListItemId === null; // no checklistItemId
+                    });;
+                    return(resultObj);
+                });
             });
         });
     },
