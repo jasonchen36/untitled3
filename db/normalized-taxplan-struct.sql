cd615da21a78e410f12a2cf747e9cd7ccae0a11f
mstumpf818 <mstumpf@ellefsontech.com>
2016-12-23 16:42:11 +0000

    tax return - updated status table with display text field

--- db/normalized-taxplan-struct.sql
+++ db/normalized-taxplan-struct.sql
@@ -186,6 +186,7 @@ CREATE TABLE `quotes_tax_returns` (
 CREATE TABLE `status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
+  `display_text` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
