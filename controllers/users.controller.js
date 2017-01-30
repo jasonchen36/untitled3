464e62dece5a122fc0919dd75981969ef539d608
Jason <jason.chen@ellefsontech.com>
2017-01-30 16:52:26 -0500

    issue has to be email and not password

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -161,7 +161,7 @@ exports.logUserIn = function(req, res, next) {
         } else if (!userObj) {
             userModel.findByEmail(req.body.email).then(function(findUser) {
                 if(!findUser){
-                    return res.status(400).json([{ msg: 'Invalid email or password' }]);
+                    return res.status(400).json([{ msg: 'Invalid email' }]);
                 }
                 if(findUser.migrated_user === 'Yes'){
                     return res.status(400).json([{ msg: 'You are a migrated user. Please reset your password.'}]);
