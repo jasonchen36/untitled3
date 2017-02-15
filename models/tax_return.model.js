0e4b52dbf5433fb1bfe7b9ec61526b8db8cd7a1b
Jason <jason.chen@ellefsontech.com>
2017-02-15 12:37:11 -0500

    added dependants info to the csv file

--- models/tax_return.model.js
+++ models/tax_return.model.js
@@ -193,7 +193,10 @@ var TaxReturn = {
                               tr.last_name, \
                               c.name AS catagory, \
                               quest.text AS question, \
-                              ans.text AS answer \
+                              ans.text AS answer, \
+                              NULL AS relationship, \
+                              NULL as birthdate, \
+                              NULL as isShared \
                             FROM users AS u \
                             JOIN accounts AS a ON a.id = u.account_id \
                             JOIN tax_returns AS tr ON tr.account_id = u.account_id \
@@ -202,8 +205,27 @@ var TaxReturn = {
                             JOIN questions AS quest ON quest.id = ans.question_id \
                             JOIN categories AS c ON c.id = quest.category_id \
                             WHERE tr.id = ? AND ans.text != "No" \
-                            ORDER BY quest.category_id';
-        return db.knex.raw(taxReturnSql, [taxReturnId]).then(function(taxReturnSqlResults) {
+                            UNION ALL \
+                            SELECT \
+                                                  tr.first_name, \
+                                                  tr.last_name, \
+                                                  c.name AS catagory, \
+                                                  quest.text AS question, \
+                                                  ans.text AS answer, \
+                                                  d.relationship AS relationship, \
+                                                  DATE_FORMAT(d.date_of_birth, "%m-%d-%Y") as birthdate, \
+                                                  d.is_shared as isShared \
+                                                FROM users AS u \
+                                                JOIN accounts AS a ON a.id = u.account_id \
+                                                JOIN tax_returns AS tr ON tr.account_id = u.account_id \
+                                                JOIN quote AS q ON q.account_id = u.account_id \
+                                                JOIN answers AS ans ON ans.tax_return_id = tr.id \
+                                                JOIN questions AS quest ON quest.id = ans.question_id \
+                                                JOIN categories AS c ON c.id = quest.category_id \
+                                                JOIN tax_returns_dependants as trd ON trd.tax_return_id = tr.id \
+						                        JOIN dependants as d ON d.id = trd.dependant_id and c.name = "Dependants" \
+                                                WHERE tr.id = ? AND ans.text != "No"'
+        return db.knex.raw(taxReturnSql, [taxReturnId, taxReturnId]).then(function(taxReturnSqlResults) {
             var taxReturnsArr = taxReturnSqlResults[0];
             return Promise.resolve(taxReturnsArr);
         });
